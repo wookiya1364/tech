0e716a52cebd96de02fbf710e47d2f2bf2902c8a
박성욱 <wookiya1364@gmail.com>
2019-04-04 18:52:34 +0900

    Updates

---
+++ precache-manifest.df6c19ba5a71f3ef16ae968b9b0a34d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tech/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "389a6e87dc16e06fe3b5",
+    "url": "/tech/static/js/runtime~main.e1a20b61.js"
+  },
+  {
+    "revision": "886006d9ee6c029181fb",
+    "url": "/tech/static/js/main.00a324fb.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/tech/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "886006d9ee6c029181fb",
+    "url": "/tech/static/css/main.f348ef2b.chunk.css"
+  },
+  {
+    "revision": "45e5013b53db19b92a9bb89af056effe",
+    "url": "/tech/index.html"
+  }
+];
